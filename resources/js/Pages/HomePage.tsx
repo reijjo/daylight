5a9408ab1e092960495a22412969208a8710d3a7
Reijjo <teemu.aitomeri@gmail.com>
2025-09-12 12:14:42 +0300

    remote git added

---
+++ resources/js/Pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-300">Home Page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ resources/js/app.tsx
@@ -0,0 +1,14 @@
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+
+createInertiaApp({
+    resolve: (name: string) => {
+        const pages = import.meta.glob("./Pages/**/*.tsx", {
+            eager: true,
+        });
+        return pages[`./Pages/${name}.tsx`];
+    },
+    setup({ el, App, props }) {
+        createRoot(el as HTMLElement).render(<App {...props} />);
+    },
+});
