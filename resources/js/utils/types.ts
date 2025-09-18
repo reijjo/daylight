38596ed884d51980f5bbffb0047ec465d4d567cc
Reijjo <teemu.aitomeri@gmail.com>
2025-09-18 16:24:16 +0300

    backend fixed for polar nights/total sun

--- resources/js/utils/types.ts
+++ resources/js/utils/types.ts
@@ -6,6 +6,17 @@ export interface FoundCity {
     lon: string;
 }
 
+interface DaylinghtDataYear {
+    year: number;
+    data: [
+        {
+            day: number;
+            date: string;
+            daylength_minutes: number;
+        }
+    ];
+}
+
 export interface DaylightData {
     id: number;
     city: string;
@@ -15,4 +26,5 @@ export interface DaylightData {
     message?: string;
     lat?: string;
     lon?: string;
+    year: DaylinghtDataYear;
 }
